77496735778526fda8fd5c1b98e64cd4421f590c
transaero21 <transaero21@elseboot.ru>
2024-11-23 22:17:16 +0300

    Init project

---
+++ app/build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    alias(libs.plugins.android.application)
+}
+
+android {
+    namespace = "ru.transaero21.simplenotes"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "ru.transaero21.simplenotes"
+        minSdk = 27
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+dependencies {
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+}
